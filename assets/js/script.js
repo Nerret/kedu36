8e1fa0a0193df822991f6424c4b650c7aab8fe9c
Nerret <peter.uffel@gmail.com>
2018-04-16 17:48:52 +0200

    sub community with js upvote done

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function arrowchange1() {
+  document.getElementsByClassName("arrowbox")[0].style.backgroundImage = "url('assets/img/arrowgreen.png')";
+}
+// shameful hardcode fix, dont have time, very sad :(
+function arrowchange2() {
+ document.getElementsByClassName("arrowbox")[1].style.backgroundImage = "url('assets/img/arrowgreen.png')";
+}
+function arrowchange3() {
+document.getElementsByClassName("arrowbox")[2].style.backgroundImage = "url('assets/img/arrowgreen.png')";
+}
+//function upvote() {
+//  document.getElementsByClassName("arrowbox").style.backgroundImage = "url('assets/img/arrowgreen.png')";
+//}
+//document.getElementsByClassName("arrowbox").onclick = function(){upvote()}
